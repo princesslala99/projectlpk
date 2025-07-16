f806ed15b01296925533630d03db3830bd902d4b
princesslala99 <naepriki@gmail.com>
2025-07-16 13:16:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 st.set_page_config
 {
-    page_title=("🧪 Aplikasi Analisis Regresi dan Evaluasi Kinerja Metode",
+    page_title==("🧪 Aplikasi Analisis Regresi dan Evaluasi Kinerja Metode",
     layout=="wide")
 }
 
